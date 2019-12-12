92b88b2391f9d52e24b4f2d45555b888f9bda3f4
Arman Riahi <namrataffy@gmail.com>
2019-12-11 23:53:54 -0800

    functionality almost complete, basic forecast done

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 var key = "d73fae2e1ac5146d1ea6fa1f709095ec";
+var m = moment();
+var todayDate = moment().format("MMMM Do YYYY");
+// check to see if cities array in local storage, if not, make an empty one
+if (!localStorage.getItem("cities")) {
+  var cities = [];
+} else {
+  var cities = JSON.parse(localStorage.getItem("cities"));
+}
 
-var cities = ["vienna", "los gatos"];
-
+// render history list of cities
 renderHistory();
 
+// on click for search button
 $(document).on("click", ".searchButton", function() {
   event.preventDefault();
   var currentCity = $("#searchBar").val();
@@ -18,20 +26,20 @@ $(document).on("click", ".searchButton", function() {
     url: queryURL,
     method: "GET"
   }).then(function(response) {
-    console.log(response);
     displayCity(response);
+    uvIndex(response);
+    forecast(response);
   });
 
   cities.push(currentCity);
-  console.log(cities);
   saveInfo();
   renderHistory();
 });
 
+// on click for city buttons
 $(document).on("click", ".cityButton", function() {
   event.preventDefault();
   var currentCity = $(this).val();
-  console.log(currentCity);
   var queryURL =
     "https://api.openweathermap.org/data/2.5/weather?q=" +
     currentCity +
@@ -42,20 +50,50 @@ $(document).on("click", ".cityButton", function() {
     url: queryURL,
     method: "GET"
   }).then(function(response) {
-    console.log(response);
     displayCity(response);
+    uvIndex(response);
+    forecast(response);
   });
 });
 
+// displays stagnent weather stats
 function displayCity(response) {
   $(".stagnentWeatherInfo").empty();
   var temp = response.main.temp;
   var wind = response.wind.speed;
-  $("#cityName").text(response.name);
+  var humidity = response.main.humidity;
+
+  $("#cityName").text(response.name + ", " + todayDate);
   $("#temp").text("Temperature is " + temp + " degrees Kelvin");
   $("#wind").text("Wind speeds of " + wind + " mph");
+  $("#humidity").text("humidity of " + humidity + " units");
 }
 
+// displays UV index with other weather stats
+function uvIndex(response) {
+  var city = response.name;
+  var lat = response.coord.lat;
+  var lon = response.coord.lon;
+  var queryURL =
+    "http://api.openweathermap.org/data/2.5/uvi?appid=" +
+    key +
+    "&lat=" +
+    lat +
+    "&lon=" +
+    lon;
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+
+    var UV = response.value;
+    $("#UV").text("UV index: " + UV);
+  });
+}
+
+// renders history cities
 function renderHistory() {
   $("#newCityArea").empty();
   for (var i = 0; i < cities.length; i++) {
@@ -74,4 +112,43 @@ function renderHistory() {
   }
 }
 
-function saveInfo() {}
+// saves cities array to local storage
+function saveInfo() {
+  localStorage.setItem("cities", JSON.stringify(cities));
+}
+
+function forecast(response) {
+  var city = response.name;
+  var lat = response.coord.lat;
+  var lon = response.coord.lon;
+  var queryURL =
+    "http://api.openweathermap.org/data/2.5/forecast?lat=" +
+    lat +
+    "&lon=" +
+    lon +
+    "&appid=" +
+    key;
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+    for (var i = 0; i < 5; i++) {
+      console.log("hey");
+      var col = $("<div>");
+      col.addClass = "col-sm-2";
+      var card = $("<div>");
+      card.addClass("card");
+      var cardBody = $("<div>");
+      cardBody.addClass("card-body");
+      var text = $("<p>");
+      text.text(response.list[i.toString()].main.temp);
+      console.log(response.list[i.toString()].main.temp);
+      cardBody.prepend(text);
+      card.prepend(cardBody);
+      col.prepend(card);
+      $("#forecast").prepend(col);
+    }
+  });
+}
